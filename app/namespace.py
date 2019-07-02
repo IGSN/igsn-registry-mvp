8d485133f6dcc42c58b7dcd88c3ceb567cb89466
Jess Robertson <jessrobertson@icloud.com>
2019-07-02 10:49:54 +0800

    Moving stuff over to other repo

--- app/namespace.py
+++
@@ -1,8 +0,0 @@
-""" file:    namespace.py (igsn-registry-mvp)
-    author:  Jess Robertson, jessrobertson@icloud.com
-    date:    July 2019
-
-    description: IGSN namespace
-"""
-# pylint: disable=C0103
-
